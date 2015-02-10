d0c6be2151d0781a8a37679fb1814758e1529fd6
Elena Torro <elenatorro@gmail.com>
2015-02-10 09:04:16 +0100

    separated from the main code

--- config/scripts/index.js
+++ config/scripts/index.js
@@ -2,4 +2,4 @@
 Spider.startWeb('canvas', 200, 200);
 document.getElementById('changeColor').addEventListener('change', function(event) {
   Spider.changeColor(Spider.hex2rgb(this.value,1));
-})
+});
