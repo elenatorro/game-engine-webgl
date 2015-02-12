e770e3b5b57c14614516960576af0a29d4f4a0a6
Elena Torro <elenatorro@gmail.com>
2015-02-12 21:36:09 +0100

    changed configuration

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-/**
- * Dependencies
- */
-var CoffeeScript= require("coffee-script");
 
-// Register CoffeeScript if exits
-if(CoffeeScript.register) CoffeeScript.register();
-
-var board = require('./lib/Board');
-
-/**
- * Exports
- */
-module.exports = board;
