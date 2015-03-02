57d6625c417bcbe82cd06bed2bd4cb01e191b386
Elena Torro <elenatorro@gmail.com>
2015-03-02 15:06:34 +0100

    trying to build the scene tree from a json

---
+++ assets/scripts/sceneTree.js
@@ -0,0 +1,22 @@
+var SceneTree = {
+  'root' : {
+    'transform1' : {
+
+    },
+
+    'mesh1' : {
+      'file'        : 'something.obj',
+      'color'       : [0,0,1,1],
+      'texturePath' : 'something.png'
+    },
+
+    'light1' : {
+       'position' : [0.0,0.0,0.0];
+       'ambient'  : [0.0,0.0,0.0,0.0];
+       'diffuse'  : [0.0,0.0,0.0,0.0];
+       'specular' : [0.0,0.0,0.0,0.0];
+    },
+
+
+  }
+}
